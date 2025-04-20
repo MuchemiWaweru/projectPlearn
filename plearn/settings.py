551affc1a67f4d2164aa9341e5d24e73d71141d5
MuchemiWaweru <petermuchemi02@gmail.com>
2025-04-20 12:44:46 +0300

    Update ALLOWED_HOSTS for Render deployment

--- plearn/settings.py
+++ plearn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d&t#$vakv#8by5e#p8%k3t%1cq59tet^e8)rcl^r$ib6oxvd^e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['projectplearn.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
