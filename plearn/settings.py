423cd149a49d495c9df2112762adc24aae190cf5
MuchemiWaweru <petermuchemi02@gmail.com>
2025-04-21 12:22:55 +0300

    white noise

--- plearn/settings.py
+++ plearn/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d&t#$vakv#8by5e#p8%k3t%1cq59tet^e8)rcl^r$ib6oxvd^e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['projectplearn.onrender.com', '127.0.0.1', 'localhost', 'projectplearn-2.onrender.com']
 
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
